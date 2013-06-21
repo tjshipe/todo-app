67d95b1e3d2b91097b03561915c67badc85f54cb
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 12:41:59 -0500

    todo done

--- todo.rb
+++ todo.rb
@@ -1,3 +1,33 @@
 require_relative 'config/application'
+require_relative '..db/config'
+require 'faker'
+
+class Todo
+  def initialize()
+
+  end
+
+  def list
+    tasks = Task.new
+    tasks.each { |task| puts task.id }
+  end
+
+  def add_task(task)
+    @tasks << Task.new([task])
+    p @tasks
+  end
+
+  def delete_task(id)
+    Task.find(id).destroy
+    # Task.destroy_all(:id => 'id')
+  end
+
+  def complete(id)
+    task = Task.find(id)
+    task.completed = 1
+    task.save
+  end
+
+end
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
